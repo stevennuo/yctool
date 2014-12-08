afac5110ed666b2ac86f07faa849e883a7196933
stevennuo <3er.com.cn@gmail.com>
2014-12-08 13:05:13 +0800

    v0.4

--- config/env/all.js
+++ config/env/all.js
@@ -3,13 +3,7 @@
 var path = require('path'),
     rootPath = path.normalize(__dirname + '/../..')
 
-var PRIVATE = require('./../private')
-var qiniu = require('qiniu')
-qiniu.conf.ACCESS_KEY = PRIVATE.qiniu.ACCESS_KEY
-qiniu.conf.SECRET_KEY = PRIVATE.qiniu.SECRET_KEY
-
 module.exports = {
     root: rootPath,
-    port: process.env.PORT || 3000,
-    qiniu: qiniu
+    port: process.env.PORT || 3000
 }
--- tool.js
+++ tool.js
@@ -11,47 +11,54 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
  */
 var express = require('express');
 var config = require('./config/config');
+
+// config
+var port = process.env.NODE_PORT || config.port;
+var PRIVATE = require('./config/ private')
+var qiniu = require('qiniu')
+qiniu.conf.ACCESS_KEY = PRIVATE.qiniu.ACCESS_KEY
+qiniu.conf.SECRET_KEY = PRIVATE.qiniu.SECRET_KEY
 var mongoose = require('mongoose');
 var db = mongoose.connect(config.db, function (err) {
     if (err) throw err;
-    var port = process.env.NODE_PORT || config.port;
-    var qiniu = config.qiniu;
 
-    //qiniuOp(qiniu, 'ghxz');
-
-    //web(port);
+    // Operation
+    qiniuOp(qiniu, 'ghxz');
+    dbOp(mongoose);
+    web(port);
 
 });
 
 // express demo
-var web = function(port){
+var web = function (port) {
     var app = express()
     app.get('/', function (req, res) {
         res.send('Hello World!')
     })
-
     var server = app.listen(port, function () {
-
         var host = server.address().address
         var port = server.address().port
-
         console.log('Example app listening at http://%s:%s', host, port)
-
     })
 }
 
 // qiniu demo
-var qiniuOp = function(qiniu, bucket){
+var qiniuOp = function (qiniu, bucket) {
     var client = new qiniu.rs.Client();
     var marker = null;
-    qiniu.rsf.listPrefix(bucket, '', marker, 100, function(err, ret) {
-        if (!err) {
-            // process ret.marker & ret.items
-            console.log(ret.marker);
-            console.log(ret.items);
-        } else {
-            console.log(err)
-            // http://docs.qiniu.com/api/file-handle.html#list
-        }
+    qiniu.rsf.listPrefix(bucket, '', marker, 100, function (err, ret) {
+        if (err) throw err;
+        // process ret.marker & ret.items
+        console.log(ret.marker);
+        console.log(ret.items);
     });
+}
+
+// mongoose demo
+var dbOp = function (mongoose) {
+    var User = mongoose.model('User', { name: String })
+    User.findOne({}, function (err, doc) {
+        if (err) throw err;
+        console.log(doc.toString())
+    })
 }
